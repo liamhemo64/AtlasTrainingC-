d7cc9b8fed3ce0d2f680710c152105164b3044ce
Liam Hemo <liamhemo64@gmail.com>
2025-12-30 13:53:31 +0200

    finished the first exercise

--- Encryptor/Encryptor/Program.cs
+++ Encryptor/Encryptor/Program.cs
@@ -1,12 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
+﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using static System.Net.Mime.MediaTypeNames;
 
-class Program
+
+class EncryptionAlgorithm
 {
+    const string PATH = @"C:\Users\Li-am Hemo\Desktop\exercise1";
     public static void Main(string[] args)
     {
         Exercise1();
@@ -18,49 +18,78 @@ class Program
             "2) Decrypte file");
         int operation = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Enter your file's path: ");
-        string path = Console.ReadLine()?.Trim(' ', '"') ?? "";
+        Console.WriteLine("Enter your file: ");
+        string file = Console.ReadLine();
 
         int key = Random.Shared.Next(1, 101);
 
         switch (operation)
         {
             case 1:
-                Program.EncryptFile(path, key);
+                EncryptFile(file, key);
                 break;
             case 2:
-                Console.WriteLine("Bring an umbrella.");
+                DecryptFile(file);
                 break;
             default:
                 Console.WriteLine("Enjoy your day!");
                 break;
         }
     }
-    public static void EncryptFile(string path, int key)
+    public static void EncryptFile(string file, int key)
     {
-        if (File.Exists(path))
+        string filePath = $@"{PATH}\{file}.txt";
+        if (File.Exists(filePath))
         {
-            string originalContent = File.ReadAllText(path);
+            string originalContent = File.ReadAllText(filePath);
             char[] chars = originalContent.ToCharArray();
 
             for (int i = 0; i < originalContent.Length; i++)
             {
-                chars[i] += (char)(chars[i] + key);
+                chars[i] = (char)(chars[i] + key);
             }
-            string newContent = new string(chars);
 
-            File.WriteAllText(path, newContent);
-            Console.WriteLine("File updated successfully!");
+            string newContent = new string(chars);  
+
+            File.WriteAllText($@"{PATH}\{file}_encrypted.txt", newContent);
+            File.WriteAllText($@"{PATH}\key.txt", key.ToString());
+            Console.WriteLine("Your new files path is: " +
+                $"\nEncrypted file : {PATH}\\{file}_encrypted.txt" +
+                $"\nKey : {PATH}\\key.txt");
         }
         else
         {
             Console.WriteLine("File not found.");
             return;
         }
-    }
 
-    public static void DecryptFile(string path, int key)
+
+    }
+    public static void DecryptFile(string file)
     {
-        // Decryption logic here
+        string filePath = $@"{PATH}\{file}_encrypted.txt";
+        if (File.Exists(filePath))
+        {
+            string originalContent = File.ReadAllText(filePath);
+            string keyString = File.ReadAllText($@"{PATH}\key.txt");
+            int key = int.Parse(keyString);
+
+            char[] chars = originalContent.ToCharArray();
+
+            for (int i = 0; i < originalContent.Length; i++)
+            {
+                chars[i] = (char)(chars[i] - key);
+            }
+            string newContent = new string(chars);
+
+            File.WriteAllText($@"{PATH}\{file}_decrypted.txt", newContent);
+            Console.WriteLine("Your new file path is: " +
+                $"\nDecrypted file : {PATH}\\{file}_decrypted.txt");
+        }
+        else
+        {
+            Console.WriteLine("File not found.");
+            return;
+        }
     }
 }
